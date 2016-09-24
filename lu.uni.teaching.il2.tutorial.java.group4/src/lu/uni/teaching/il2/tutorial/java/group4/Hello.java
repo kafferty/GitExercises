351aa4bf17318e1955d5e9ae98cc224b1df0c507
AndreiChugunov <bloodyavangard@mail.ru>
2016-09-24 18:47:17 +0300

    New println has added

--- lu.uni.teaching.il2.tutorial.java.group4/src/lu/uni/teaching/il2/tutorial/java/group4/Hello.java
+++ lu.uni.teaching.il2.tutorial.java.group4/src/lu/uni/teaching/il2/tutorial/java/group4/Hello.java
@@ -4,8 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.print("Hello world!");
-		System.out.print("Meow");
-
+		System.out.println("Meow");
+		System.out.println("tu-tu");
 	}
 
 }
